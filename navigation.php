9afea239bd024437d97ce51c98fc47e9836ff07e
Tamás Nagy <ntamasalbert@gmail.com>
2024-12-23 08:56:40 +0100

    finish navigation bar

--- navigation.php
+++ navigation.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-    <div class="container d-flex flex-row align-items-center">
+<nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+    <div class="container m-0">
         <a href="index.php" class="navbar-brand text-primary">iKarRental</a>
         <ul class="navbar-nav ml-auto d-flex flex-row align-items-center">
             <li class="nav-item font-weight-bold">
                 <a href="login.php" class="nav-link text-primary">Bejelenkezés</a>
             </li>
             <li class="nav-item text-dark bg-primary font-weight-bold rounded-pill p-2">
-                <a href="" class="nav-link">Regisztráció</a>
+                <a href="register.php" class="nav-link">Regisztráció</a>
             </li>
         </ul>
     </div>
